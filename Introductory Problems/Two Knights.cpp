ed2d7dcef8ef352aeaa7a77cf63e5e5ea6a1c237
Deepak Kumar <deepakku28@gmail.com>
2021-01-29 19:29:11 +0530

    Create Two Knights.cpp

---
+++ Introductory Problems/Two Knights.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+int main(){
+	ll n;
+	cin>>n;
+	for(ll i=1;i<=n;i++){
+		cout<<((i*i)*((i*i)-1))/2-(4*(i-1)*(i-2))<<endl;
+	}
+}
