1c0f9dc273220002b7bc3bd7e667ec2b1e1771c6
Deepak Kumar <deepakku28@gmail.com>
2021-01-30 18:28:47 +0530

    Create Coin Combinations I.cpp

---
+++ Dynamic Programming/Coin Combinations I.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+ll mod=1e9+7;
+ 
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	ll n,target;
+	cin>>n>>target;
+	vector<ll> val(n);
+	for(ll &it:val){
+		cin>>it;
+	}
+	vector<ll> dp(target+1,0);
+	dp[0]=1;
+	for(ll i=1;i<=target;i++){
+		for(ll j=0;j<n;j++){
+			if(i-val[j]>=0){
+				dp[i]=(dp[i]+dp[i-val[j]]);
+				if(dp[i]>=mod){
+					dp[i]-=mod;
+				}
+			}
+		}
+	}
+	cout<<dp[target]<<endl;
+	return 0;
+}
